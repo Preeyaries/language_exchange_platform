67dac7f9a3b697655e52d487cb1548d95e1578db
Preeyanan <ploy.skp25@gmail.com>
2026-04-03 03:35:34 +1000

    update UI and BottomNav pages

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     //baseURL: 'http://localhost:5000', // local
-    baseURL: 'http://3.24.242.2:5000', // live
+    baseURL: 'http://13.210.234.249:5000', // live
     headers: { 'Content-Type': 'application/json' },
 });
 
--- frontend/src/axoisConfig.jsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-    //baseURL: 'http://localhost:5000', // local
-    baseURL: 'http://3.24.242.2:5000', // live
-    headers: { 'Content-Type': 'application/json' },
-});
-
-export default axiosInstance;
