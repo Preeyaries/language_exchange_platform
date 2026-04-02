ce26f7d3e8c4d19c2f5c0629bf41ff71060f08af
Preeyanan <ploy.skp25@gmail.com>
2026-04-03 04:37:55 +1000

    fix: allow multiple CORS origins for local and production

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,24 @@ const messageRoutes = require("./routes/messageRoutes");
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://13.210.234.249",
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
+
 app.use(cors({
   origin: process.env.CLIENT_URL || "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
