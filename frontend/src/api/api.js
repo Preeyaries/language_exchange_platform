6298598c85cb1825d1ffbac13b0d341af7cc2b85
Preeyanan Khamfoei <ploy.skp25@gmail.com>
2026-04-13 14:56:02 +1000

    config: update API URL to use env variable, set new EC2 IP

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
-
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
@@ -11,5 +9,4 @@ API.interceptors.request.use((req) => {
   }
   return req;
 });
-
 export default API;
--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     //baseURL: 'http://localhost:5000', // local
-    baseURL: 'http://3.24.242.2:5000', // live
+    baseURL: 'http://3.27.235.128:5000', // live
     headers: { 'Content-Type': 'application/json' },
 });
 
